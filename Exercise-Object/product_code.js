2cd888c46810389f74e7be6b89cdff2284bdc9cd
AbelZewdie <Abelmzewdie@gmail.com>
2024-05-08 22:37:53 -0400

    Product Code

---
+++ Exercise-Object/product_code.js
@@ -0,0 +1,12 @@
+let parsePartCode = {
+supplierCode : "34fg3",
+productNumber : "65458",
+size : "XL"
+}
+
+console.log("Supplier Code:", parsePartCode.supplierCode)
+console.log("product Number:", parsePartCode.productNumber)
+console.log("Size:", parsePartCode.size)
+
+console.log(" ");
+console.log(" ");
